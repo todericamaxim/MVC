4e4979502822e7ac4244d35ce3f5e82fd8a8eb82
todericamaxim <56884131+todericamaxim@users.noreply.github.com>
2020-01-21 18:01:24 +0200

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exemplu_MVC.Models;
+
+namespace Exemplu_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentData studentData;
+        public StudentController()
+        {
+            this.studentData = new StudentData();
+        }
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> students = this.studentData.GetStudents();
+            return this.View(students);
+        }
+        public ActionResult ById(int id)
+        {
+            var u = this.studentData.GetStudent(id);
+            return this.View(u);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exemplu_MVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    public class StudentData
+    {
+        private static readonly List<Student> students = new List<Student>();
+
+        static StudentData()
+        {
+            students.Add(new Student { Id = 1, Name = "Ion",Age=12 });
+            students.Add(new Student { Id = 2, Name = "Valeriu",Age=20 });
+            students.Add(new Student { Id = 3, Name = "Mihai", Age=21 });
+            students.Add(new Student { Id = 3, Name = "Andrei", Age=25 });
+            students.Add(new Student { Id = 3, Name = "Maria", Age=27 });
+        }
+        public StudentData()
+        { }
+        public List<Student> GetStudents()
+        { return students; }
+
+        public Student GetStudent(int Id)
+        {
+            return students.Find(x => x.Id == Id);
+        }
+    }
+}
